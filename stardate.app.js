672ad3ee827bc66c477f728bd7a52a1ab6ad2b81
Robert Kaiser <kairo@kairo.at>
2021-07-27 01:35:07 +0200

    create an upper and a lower section (lower one will hold 'normal' date/time)

--- stardate.app.js
+++ stardate.app.js
@@ -11,26 +11,27 @@ const hbarHt = 5;
 const outRad = 25;
 const inRad = outRad - hbarHt;
 const gap = 3;
-const sbarGapPos = 80;
+const divisionPos = 60;
+const sbarGapPos = 150;
 
 // Star Trek famously premiered on Thursday, September 8, 1966, at 8:30 p.m.
 // See http://www.startrek.com/article/what-if-the-original-star-trek-had-debuted-on-friday-nights
 const gSDBase = new Date("September 8, 1966 20:30:00 EST");
-const sdatePosTop = hbarHt + 25;
+const sdatePosTop = 10;
 const sdatePosLeft = sbarWid + 25;
+const sdateDecimals = 4;
+const secondsPerYear = 86400 * 365.2425;
+const sdateDecFactor = Math.pow(10, sdateDecimals);
+
 
 function updateStardate() {
   // work out how to display the current time
   var curDate = new Date();
 
-  var secondsPerYear = 86400 * 365.2425;
-  var SDDecimals = 3;
-  var decFactor = Math.pow(10, SDDecimals);
-
   // Note that the millisecond division and the 1000-unit multiplier cancel each other out.
   var sdateval = (curDate - gSDBase) / secondsPerYear;
 
-  var sdatestring = (Math.floor(sdateval * decFactor) / decFactor).toFixed(SDDecimals);
+  var sdatestring = (Math.floor(sdateval * sdateDecFactor) / sdateDecFactor).toFixed(sdateDecimals);
 
   // Reset the state of the graphics library
   g.reset();
@@ -40,15 +41,14 @@ function updateStardate() {
   //g.setBgColor("#FF6600"); // for debugging
   g.clearRect(sdatePosLeft,
               sdatePosTop,
-              sdatePosLeft + g.stringWidth(sdatestring) + 1,
+              sdatePosLeft + g.stringWidth(sdatestring) + 3,
               sdatePosTop + g.getFontHeight());
   // draw the current time
   g.setColor("#9C9CFF");
   g.drawString(sdatestring, sdatePosLeft, sdatePosTop);
 
-
   // Schedule next when an update to the last decimal is due.
-  var mstonextUpdate = (Math.ceil(sdateval * decFactor) / decFactor - sdateval) * secondsPerYear;
+  var mstonextUpdate = (Math.ceil(sdateval * sdateDecFactor) / sdateDecFactor - sdateval) * secondsPerYear;
   if (redrawClock) {
     setTimeout(updateStardate, mstonextUpdate);
   }
@@ -56,21 +56,35 @@ function updateStardate() {
 
 // Clear the screen once, at startup.
 g.clear();
-// Draw fancy borders.
-// First, create the rounded corner.
-g.setColor("#E7ADE7");
-g.fillCircle(outRad,outRad,outRad);
-g.fillRect(outRad,0,outRad*2,outRad);
-g.fillRect(outRad,0,sbarWid+outRad,hbarHt); // top bar stub
-g.fillRect(0,outRad,sbarWid,sbarGapPos); // side bar
+// Draw LCARS borders.
+// More colors: teal #008484, yellow FFCF00, purple #6050B0
+// Upper section: rounded corner.
+g.setColor("#A09090");
+g.fillCircle(outRad,divisionPos-outRad,outRad);
+g.fillRect(outRad,divisionPos-outRad,outRad*2,divisionPos);
+g.fillRect(outRad,divisionPos-hbarHt,sbarWid+outRad,divisionPos); // div bar stub
+g.fillRect(0,0,sbarWid,divisionPos-outRad); // side bar
 g.setColor("#000000"); // blocked out areas of corner
-g.fillCircle(sbarWid+inRad+1,hbarHt+inRad+1,inRad);
-g.fillRect(sbarWid+1,hbarHt+inRad,sbarWid+outRad,outRad*2);
-// top bar
+g.fillCircle(sbarWid+inRad+1,divisionPos-hbarHt-inRad-1,inRad);
+g.fillRect(sbarWid+1,divisionPos-outRad*2,sbarWid+outRad,divisionPos-hbarHt-inRad);
+// upper division bar
+g.setColor("#A06060");
+g.fillRect(sbarWid+outRad+gap+1,divisionPos-hbarHt,g.getWidth(),divisionPos);
+// Lower section: rounded corner.
+const lowerTop = divisionPos+gap+1;
+g.setColor("#E7ADE7");
+g.fillCircle(outRad,lowerTop+outRad,outRad);
+g.fillRect(outRad,lowerTop,outRad*2,lowerTop+outRad);
+g.fillRect(outRad,lowerTop,sbarWid+outRad,lowerTop+hbarHt); // div bar stub
+g.fillRect(0,lowerTop+outRad,sbarWid,sbarGapPos); // side bar
+g.setColor("#000000"); // blocked out areas of corner
+g.fillCircle(sbarWid+inRad+1,lowerTop+hbarHt+inRad+1,inRad);
+g.fillRect(sbarWid+1,lowerTop+hbarHt+inRad,sbarWid+outRad,lowerTop+outRad*2);
+// lower division bar
 g.setColor("#FF9F00");
-g.fillRect(sbarWid+outRad+gap+1,0,g.getWidth(),hbarHt);
+g.fillRect(sbarWid+outRad+gap+1,lowerTop,g.getWidth(),lowerTop+hbarHt);
 // second color of side bar
-g.setColor("#008484");
+g.setColor("#C09070");
 g.fillRect(0,sbarGapPos+gap+1,sbarWid,g.getHeight());
 // Draw immediately at first.
 updateStardate();
